5a6a9ebec34a4d83637b1b898bdccf13c44e55e6
Nitesh Bhardwaj <kumarnitesh52864@gmail.com>
2026-04-20 01:46:33 +0530

    updated

--- render_start.sh
+++ render_start.sh
@@ -6,9 +6,9 @@ set -o errexit
 echo "Running database migrations..."
 python manage.py migrate
 
-# Seed the required data into the database
-echo "Seeding counsellors..."
-python manage.py seed_counsellors
+# # Seed the required data into the database
+# echo "Seeding counsellors..."
+# python manage.py seed_counsellors
 
 # Start the gunicorn server
 echo "Starting Gunicorn server..."
